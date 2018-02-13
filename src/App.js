5f5b736a0389afd3b43b11df6855d16b0a59d8d1
Johnny <emailjchien@gmail.com>
2018-02-13 09:00:57 -0800

    React Native app initial setup.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './components/common';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header>
+          FirebaseAuthentication
+        </Header>
+        <Text>An App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
